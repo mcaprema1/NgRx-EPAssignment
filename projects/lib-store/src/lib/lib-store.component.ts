5a997e74bd9f1cd3687c2dfc1350260580691c6e
mcaprema1 <mcaprema1@gmail.com>
2023-03-22 17:54:45 +0530

    commit on 22.3.23

---
+++ projects/lib-store/src/lib/lib-store.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-lib-store',
+  template: `
+    <p>
+      lib-store works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LibStoreComponent {
+  
+
+}
